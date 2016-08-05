be678c7833fc05574e1d65cddab08d2ce8e7296c
Kelly Mason <mason.kelly.m@gmail.com>
2016-08-04 23:07:47 -0700

    app.jsx require jsuri

---
+++ client/components/layout/App.jsx
@@ -0,0 +1,55 @@
+var React = require('react');
+var Reqwest = require('reqwest');
+var BlabsView = require('../blabs/View.jsx');
+var Menu = require('./Menu.jsx');
+var Router = require('react-router');
+var RouteHandler = Router.RouteHandler;
+var Uri = requre('jsuri');
+
+module.exports = React.createClass({
+	getDefaultProps: function() {
+		return {origin: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : ''};
+	},
+	getInitialState: function() {
+		return {showMenu: false, signedIn: false, currentUser: {handle: ''}};
+	},
+	componentWillMount: function(){
+	var jwt = new Uri(location.search).getQueryParamValue('jwt');
+	},
+	componentDidMount: function() {
+		if (!!sessionStorage.getItem('jwt')) {this.currentUserFromAPI();}
+	},
+	currentUserFromAPI: function() {
+		this.readFromAPI(this.props.origin + '/current_user', function(user) {
+			this.setState({signedIn: true, currentUser: user});
+		}.bind(this));
+	},
+	readFromAPI: function(url, successFunction) {
+		Reqwest({
+			url: url,
+			type: 'json',
+			method: 'get',
+			contentType: 'application.json',
+			success: successFunction,
+			error: function(error) {
+				console.error(url, error['response']);
+				location = '/';
+			}
+		});
+	},
+	handleMenuClick: function() {
+		this.setState({showMenu: !this.state.showMenu});
+	}
+	render: function () {
+		var menu = this.state.showMenu ? 'show-menu' : 'hide-menu';
+
+		return (
+			<div id="app" className={menu}>
+				<Menu sendMenuClick={this.handleMenuClick} />
+				<div id="content">
+					<RouteHandler origin={this.props.origin} readFromAPI={this.readFromAPI} />
+				</div>	
+			</div>
+		);
+	}
+});
